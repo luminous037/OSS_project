657d4c9a26b5b90aec31be80d941cfc5ef944a57
minseonju <10sc1108@naver.com>
2024-04-12 21:22:49 +0900

    [ADD]물웅덩이에 wave animation 추가

--- client/src/components/start.js
+++ client/src/components/start.js
@@ -17,10 +17,12 @@ function Start() {
   useEffect(() => {
     const interval = setInterval(() => {
       setDrops(drops => [...drops, <Drop key={drops.length} />]);
-    }, 1000); // 1초마다 새로운 물방울 생성
+    }, 2000); 
 
     return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
   }, []);
+
+  
 
   return (
     <div className="start-page">
@@ -28,6 +30,9 @@ function Start() {
       <div className="logo">Meddy Baby</div>
       {drops}
       <img src={chick} alt="chick" className="chick" />
+      <div className="ocean">
+      <div className="wave"></div>
+    </div>
     </div>
   );
 }
