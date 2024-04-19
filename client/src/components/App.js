3c56062abf2ef789381f04205768b4b4212655f3
Jiji <luminous037@gmail.com>
2024-04-19 12:39:48 +0900

    [CHORE] App.js 내의 Infopage 경로 변경

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './NavigationBar.js'; // NavigationBar 컴포넌트를 임포트합니다.
 import Start from './Start.js'; // 홈 페이지 컴포넌트
-import InfoPage_1 from './InfoPage_1';
-import InfoPage_2 from './InfoPage_2';
-import InfoPage_3 from './InfoPage_3';
+import InfoPage_1 from './InfoPage/InfoPage_1.js';
+import InfoPage_2 from './InfoPage/InfoPage_2.js';
+import InfoPage_3 from './InfoPage/InfoPage_3.js';
 import Main from './Main.js';
-
-import WeeklyCheck from './WeeklyCheck.js';
 
 function App() {
 
@@ -22,7 +20,6 @@ function App() {
           <Route path="/InfoPage_1" element={<InfoPage_1 />} />
           <Route path="/InfoPage_1/InfoPage_2" element={<InfoPage_2 />} />
           <Route path="/InfoPage_1/InfoPage_2/InfoPage_3" element={<InfoPage_3 />} />
-          <Route path="WeeklyCheck" element={<WeeklyCheck />} />
       </Routes>
         <NavigationBar /> {/* 하단 네비게이션 바를 추가합니다. */}
       </div>
--- client/src/components/InfoPage/InfoPage_1.js
+++ client/src/components/InfoPage/InfoPage_1.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import './App.css';
 import { Link } from 'react-router-dom';
+import 'InfoPage.css';
 
 
 function InfoPage_1() {
