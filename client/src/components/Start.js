12dcf81a8ae020994c2d5b7407f84b7f8e2947d1
minseonju <10sc1108@naver.com>
2024-05-01 20:19:52 +0900

    [Feature]병아리 주위에 알약이 생기는 애니메이션 구현

--- client/src/components/Start.js
+++ client/src/components/Start.js
@@ -6,6 +6,7 @@ import puddle from './puddle.png';
 import pill1 from './pill1.png';
 import pill2 from './pill2.png';
 import jam from './jam.png';
+import flower from './flower.png';
 
 
 
@@ -16,12 +17,12 @@ function Start() {
     let vh = 0;
     vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-    const pillImages = [pill1, pill2, jam]; // 이 배열에 필요한 만큼 알약 이미지를 추가하세요.
+    const pillImages = [pill1, pill2, jam,flower]; // 이 배열에 필요한 만큼 알약 이미지를 추가하세요.
     let delay = 0;
     
     pillImages.forEach((pill, index) => {
       setTimeout(() => {
-        setVisiblePills((currentPills) => [...currentPills, <img key={index} src={pill} alt={`pill${index+1}`} className={`pill${index+1}`} style={{position: 'fixed', left: `${10 + 20*index}%`, bottom: '10%'}}/>]);
+        setVisiblePills((currentPills) => [...currentPills, <img key={index} src={pill} alt={`pill${index+1}`} className={`pill${index+1}`} style={{position: 'absolute', left: `${15 + 20*index}%`, bottom: '30%', width: '40px', height: '40px'}}/>]);
       }, 1000 + delay); // 1초 간격으로 알약 이미지가 나타납니다.
       delay += 1000; // 다음 알약 이미지가 나타나기까지의 시간 간격을 조정합니다.
     });
@@ -34,7 +35,11 @@ function Start() {
     
     <div className="start-page">
       <div className="top-section">
+      <div class="puddle-top-box"></div>
       <img src={puddle} alt="puddle" className="puddle-animation" />
+      <div className="ocean">
+      <div className="wave"></div>
+    </div>
 
     
       </div>
