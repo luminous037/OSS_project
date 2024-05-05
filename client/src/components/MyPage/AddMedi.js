ca8329a337d51370f460b9759841183f8917a374
GAUDUL <limjsu12@naver.com>
2024-05-05 17:56:43 +0900

    [FEAT] 약 추가 시 정보 데이터베이스에 저장

--- client/src/components/MyPage/AddMedi.js
+++ client/src/components/MyPage/AddMedi.js
@@ -1,33 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 
 function AddMedi() {
+    
     const [mediData, setMediData] = useState({ //약 정보 저장할 배열
         mediName: '',
         time: '',
         detail: ''
     });
 
-    useEffect(() => { //post 요청
+    const fetchData = () => { // fetch 요청 함수
         fetch('http://localhost:4000/addList', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json' //JSON 형식으로 전송
+                'Content-Type': 'application/json' // JSON 형식으로 전송
             },
-            body: JSON.stringify(mediData) //약 정보를 body에 저장
+            body: JSON.stringify(mediData) // 약 정보를 body에 저장
         })
         .then(response => {
-            if (response.ok) { //서버 연결 성공 시
+            if (response.ok) { // 서버 연결 성공 시
                 return response.text();
             }
             throw new Error('서버 연결 실패');
         })
-        .then(data => { //데이터 확인용
+        .then(data => { // 데이터 확인용
             console.log(data);
         })
         .catch(err => {
-            console.error(err);
+            console.error('fetchData 중 오류: ',err);
         });
-    }, []); //컴포넌트가 마운트될 때 한 번만 실행되도록 설정
+    };
 
     const handleChange = (e) => { //데이터 입력 시
         setMediData({
@@ -41,15 +42,7 @@ function AddMedi() {
             <input type="text" name="mediName" value={mediData.mediName} onChange={handleChange} />
             <input type="text" name="time" value={mediData.time} onChange={handleChange} />
             <input type="text" name="detail" value={mediData.detail} onChange={handleChange} />
-            <button onClick={() => console.log(mediData)}>확인</button>
-
-            {/* mediData 값 확인 */}
-            <div>
-            <p>mediName: {mediData.mediName}</p>
-            <p>time: {mediData.time}</p>
-            <p>detail: {mediData.detail}</p>
-            </div>
-            
+            <button onClick={fetchData}>확인</button> 
         </div>  
     );
 }
--- server/Server.js
+++ server/Server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const { dbConnect, getDatabase } = require('./DbConnect');
-const { MongoClient } = require('mongodb');
 
 const port = process.env.PORT || 4000; //서버 포트 번호
 const fs =require('fs');
@@ -18,24 +17,6 @@ app.listen(port, () => {
     dbConnect(); //DB 연결
 }); 
 
-
-// function mediShow(){ //약 목록이 보여야 할 때 -> mypage 들어갈 때, 약 목록 갱신될 때 -> 약 추가하거나 삭제했을 때
-    
-//     const database = getDatabase(); //db 가져오기
-
-//     const mediListcollection = database.collection("medicineList"); //약의 이름, 복용 시간 등
-
-//     // db내의 모든 mediName을 가져와서 queryResult에 저장
-//     mediListcollection.find({}, { projection: { _id: 0, mediName: 1 } }).toArray()
-//         .then((queryResult) => {
-//             app.get('/list', (req, res) => { //list 경로에 있는 약 목록
-//                 res.send(queryResult) //json형태로 변경 후 전송
-//             });
-//         })
-//         .catch((err) => {
-//             console.error('mediShow Error: ',err);
-//         });
-// }
 
 app.get('/list', (req, res) => {
     const database = getDatabase(); //db 가져오기
@@ -57,16 +38,13 @@ app.post('/addList', (req, res)=>{ //약 추가할 때
 
     const mediListcollection = database.collection("medicineList"); //컬렉션 참조
 
-    const mediname = req.body.mediName;
-    const time = req.body.time;
-    const detail = req.body.detail;
-
+    const {mediName, time, detail}=req.body;
     mediListcollection.insertOne({ //db에 내용 삽입
-        'mediName' : mediname,
+        'mediName' : mediName,
         'time' : time,
         'detail' : detail
         })        
-        .then((result) => {
+        .then((result) => { //데이터 확인
             console.log(result);
         })
     .catch((err) => { //에러 발생 시
