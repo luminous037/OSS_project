b4905a5f2f843cd1dd5546396a9bcd2d288c9a3b
Jiji <luminous037@gmail.com>
2024-05-07 08:51:44 +0900

    [FEAT] Info 1 CSS작성

--- client/src/components/InfoPage/InfoPage_1.js
+++ client/src/components/InfoPage/InfoPage_1.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import 'InfoPage.css';
-
+import './InfoPage_1.css';
 
 function InfoPage_1() {
   const [childName, setChildName] = useState('');
 
-  
   return (
-    <div className="App">
-      Meddy Baby
+    <div className="Page1">
+      
+      <h1
+      style={{
+        color: "#f00",
+        borderRight: "10px",
+        marginBottom: "30px",
+        opacity: 1.0,
+      }}
+      >
+        MeddyBaby
+      </h1>
       <p>
-        우리 아이 이름은{' '}
-        <input
+        우리아이 이름은
+      </p>
+
+      <div classNmae="nameBlank">
+      <input
           type="text"
           value={childName}
           onChange={(e) => setChildName(e.target.value)}
-        />{' '}
+          />
+      </div>
+
+      <p>
         어린이
       </p>
-      
+
       <div className="navigator">
-       <Link to="./InfoPage_2" className="nav-item">다음</Link>
+        <Link to="./InfoPage_2" className="nav-item">다음</Link>
       </div>
     </div>
   );
 }
+
 
 export default InfoPage_1;
