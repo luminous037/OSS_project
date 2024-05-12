ea85f22240ca1fbba77d8cc431b9b1df8c3a76eb
minseonju <10sc1108@naver.com>
2024-05-12 19:47:15 +0900

    [CHORE]페이지index 값에 따라 이미지 index 값도 변경하여 페이지에 맞는 이미지 나타나도록 구현

--- client/src/components/mainPage/Guidebook.js
+++ client/src/components/mainPage/Guidebook.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import './Guidebook.css';
-import chick from '../image/chick.png';
+
+import jam from '../image/jam.png';
+import flower from '../image/flower.png';
 
 const slides = [
   '메인 페이지.',
   '상점 페이지.',
   '설정 페이지.',
   '부가 페이지.'
-  // 필요한 만큼 페이지(슬라이드)를 추가하세요.
+  
 ];
 
 const InstructionModalWithSlide = ({ isOpen, close }) => {
@@ -23,31 +25,40 @@ const InstructionModalWithSlide = ({ isOpen, close }) => {
     setCurrentSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length);
   };
 
- // JSX 요소 내 스타일 적용 예시
-return (
-  isOpen && (
-    <div className="modalBackground">
-      <div className="modalContent">
-        <button className="closeButton" onClick={close}>&times;</button>
-        <h2>간단한 사용 설명서
-        </h2>
-        <p>{slides[currentSlide]}</p>
-        <div className="middle-section">
-       
+  // 현재 슬라이드에 맞는 이미지를 선택하는 함수
+  const getImageForCurrentSlide = () => {
+    switch (currentSlide) {
+      case 0:
+        return flower;
+      case 1:
+        return jam;
+      
+      default:
+        return null;
+    }
+  };
 
-        </div>
-
-        
-        <div className="buttonGroup">
-          <button className="button" onClick={prevSlide}>Previous</button>
-          <button className="button" onClick={close}>Close</button>
-          <button className="button" onClick={nextSlide}>Next</button>
+  return (
+    isOpen && (
+      <div className="modalBackground">
+        <div className="modalContent">
+          <button className="closeButton" onClick={close}>&times;</button>
+          <h2>간단한 사용 설명서</h2>
+          <p>{slides[currentSlide]}</p>
+          <div className="middle-section">
+            {/* 현재 슬라이드에 맞는 이미지를 표시 */}
+            <img src={getImageForCurrentSlide()} alt={`Slide ${currentSlide}`} className="slideImage" />
+          </div>
+          <div className="buttonGroup">
+            <button className="button" onClick={prevSlide}>Previous</button>
+            <button className="button" onClick={close}>Close</button>
+            <button className="button" onClick={nextSlide}>Next</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
-);
-
+    )
+  );
 };
 
 export default InstructionModalWithSlide;
+
