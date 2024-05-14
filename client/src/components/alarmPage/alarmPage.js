e8074776e4cb594f8cc3a586cbc9cef0a9f93930
minseonju <10sc1108@naver.com>
2024-05-14 18:40:05 +0900

    [Feature]화면 상중하로 분할

--- client/src/components/alarmPage/alarmPage.js
+++ client/src/components/alarmPage/alarmPage.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import './alarmPage.css'; 
+import './alarmPage.css';
 
 const AlarmPage = () => {
   return (
     <div className="alarm-container">
-        <div>alarm page</div>
-      <div className="animated-circle">
-        
+
+      <div className="top-container">
       </div>
       
+
+      <div className="middle-container">
+        <div className="animated-circle">
+
+        </div>
+      </div>
+      
+      <div className="bottom-container">
+      </div>
     </div>
   );
 };
 
 export default AlarmPage;
+
