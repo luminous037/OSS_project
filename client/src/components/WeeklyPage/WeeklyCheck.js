fc8686a797ab3526fb861370257bbbf5452b5e00
Jiji <luminous037@gmail.com>
2024-05-14 23:20:02 +0900

    [feat] 배경이미지 css, 판넬 이미지 css 해상도에 따라 이미지 비율이 조정되도록 작성

--- client/src/components/WeeklyPage/WeeklyCheck.js
+++ client/src/components/WeeklyPage/WeeklyCheck.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import './WeeklyPage.css';
+import pannel from '../image/pannel.png';
+
+import chick1 from '../image/chick1.png';
+import chick2 from '../image/chick2.png';
 
 function WeeklyCheck() {
   const [days, setDays] = useState(0);
@@ -14,7 +18,7 @@ function WeeklyCheck() {
   const updateInputFields = (numDays) => {
     const newInputFields = [];
     for (let i = 0; i < numDays; i++) {
-      newInputFields.push({ id: i, imageSrc: `image_${i + 1}.jpg` });
+      newInputFields.push({ id: i });
     }
     setInputFields(newInputFields);
   };
@@ -30,6 +34,22 @@ function WeeklyCheck() {
       <div className="background">
         <h1></h1>
       </div>
+
+      <div className="title">
+        칭찬스티커
+      </div>
+
+      <div className="pannelImage">
+        <img src={pannel} className="pannel" />
+      </div>
+
+      <div className="chick1">
+        <img src={chick1} className="chick1"/>
+      </div>
+      <div className="chick2">
+        <img src={chick2} className="chick2"/>
+      </div>
+
 
       <div className="howMuchStamp">
         <input
