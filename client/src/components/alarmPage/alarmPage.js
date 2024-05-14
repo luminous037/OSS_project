13e1eb62fe2e48fd3b802d8415c8f22db9d2b9ae
minseonju <10sc1108@naver.com>
2024-05-14 19:48:45 +0900

    [Feature]애니메이션 원 안에 텍스트 배치

--- client/src/components/alarmPage/alarmPage.js
+++ client/src/components/alarmPage/alarmPage.js
@@ -3,12 +3,23 @@ import './alarmPage.css';
 import chick2 from '../image/chick2.png';
 
 const AlarmPage = () => {
+
+    const textStyle = {
+        fontSize: '50px', 
+        fontFamily: 'on', 
+        color: 'black', 
+      };
+
   return (
     <div className="alarm-container">
 
       <div className="top-container">
       <div className="animated-circle"> 
       <img src={chick2} alt="chick2" className="chick2" />
+      <div className='wirting-box'>
+      <div className="writing" style={textStyle}>약 먹을 시간이에요!</div>
+      </div>
+      
         </div>
       </div>
       
