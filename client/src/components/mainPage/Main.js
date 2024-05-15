e3cb6d81110b1da0179ae8340592c89450c6a279
minseonju <10sc1108@naver.com>
2024-05-15 18:42:41 +0900

    [Feature]화면 상단 공간 할당

--- client/src/components/mainPage/Main.js
+++ client/src/components/mainPage/Main.js
@@ -1,10 +1,9 @@
-// App.js 또는 메인 페이지의 컴포넌트 파일에 아래 코드 추가
 import React, { useState, useEffect } from 'react';
 import InstructionModal from './Guidebook.js';
+import './Main.css';
 
 const MainPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  
 
   useEffect(() => {
     // 페이지가 로드될 때 모달을 자동으로 열고 싶다면 아래 코드 활성화
@@ -12,14 +11,15 @@ const MainPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>메인 페이지</h1>
-      <InstructionModal isOpen={isModalOpen} close={() => setIsModalOpen(false)} />
-      {}
-      <button onClick={() => setIsModalOpen(true)}>사용 설명 보기</button>
+    <div className='mainpage-all-container'>
+      <div className='mainpage-top-container'>
+        <h1>메인 페이지</h1>
+        <InstructionModal isOpen={isModalOpen} close={() => setIsModalOpen(false)} />
+        <button onClick={() => setIsModalOpen(true)}>사용 설명 보기</button>
+      </div>
+
     </div>
   );
 };
 
 export default MainPage;
-
