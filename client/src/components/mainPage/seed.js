a953d940a83983645a853cd75d4323aec486ae26
minseonju <10sc1108@naver.com>
2024-05-16 19:51:34 +0900

    [ADD]모달 창 구현 코드 추가

--- client/src/components/mainPage/seed.js
+++ client/src/components/mainPage/seed.js
@@ -17,6 +17,43 @@ function Seed() {
     setIsModalOpen(!isModalOpen);
   };
 
+  const selectSeed = (seed) => {
+    setSelectedSeed(seed);
+    setIsSprouted(false);
+    setIsSeedPlanted(true); // 씨앗 심기 상태를 true로 설정
+    toggleModal();
+  };
+
+
+
+
+
+
+
+
+  return (
+    <div className="Seed">
+      <button onClick={toggleModal}>씨앗 심기</button>
+      {isModalOpen && (
+        <div className="modal">
+          <h2>씨앗 선택</h2>
+          <ul>
+            {seeds.map((seed) => (
+              <li key={seed.id} onClick={() => selectSeed(seed)} className="seed-list-item">
+                <div className="seed-image-container">
+                  <img src={seed.imageUrl} alt={`${seed.name} 이미지`} className="seed-image"/>
+                </div>
+                {seed.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+</div>
+  );
+
+
 }
 
 export default Seed;
