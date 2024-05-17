aa58ebfd317f04665c7a136fdcf028547d10dc6a
GAUDUL <limjsu12@naver.com>
2024-05-17 19:04:48 +0900

    [FEAT] Save MediDetail Data in DB

--- client/src/components/MyPage/AddMedi.js
+++ client/src/components/MyPage/AddMedi.js
@@ -1,134 +1,149 @@
-import { useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './DetailPage.css';
 
 function AddMedi() {
-    
-    const [mediData, setMediData] = useState({ //약 정보 저장할 배열
+    const [mediData, setMediData] = useState({ //기본 데이터 저장 구조
         mediName: '',
         time: '',
-        detail: ''
+        detail: {
+            morning: false,
+            afternoon: false,
+            evening: false,
+            before: false,
+            after: false,
+            time: ''
+        }
     });
 
+    const navigate = useNavigate();
 
-    const navigate=useNavigate();
-    const goToMypage=()=>{ //마이페이지 이동
+    const goToMypage = () => { //mypage 이동
         navigate('/MyPage');
     };
 
-    const fetchData = () => { // fetch 요청 함수
+    const fetchData = (data) => { //데이터 저장
         fetch('http://localhost:4000/addList', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json' // JSON 형식으로 전송
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify(mediData) // 약 정보를 body에 저장
-            
+            body: JSON.stringify(data)
         })
-        .then( ()=>{goToMypage();})
+        .then(() => {
+            goToMypage();
+        })
         .catch(err => {
-            console.error('fetchData 중 오류: ',err);
+            console.error('fetchData 중 오류: ', err);
         });
     };
 
-    const handleChange = (e) => { //데이터 입력 시
+    const handleChange = (e) => { //medidata 이름, 시간 수정
         setMediData({
             ...mediData,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         });
     };
+
+    const handleSave = () => { //medidata 복용법 수정
+        const infoData = {
+            morning: buttonStates.morning,
+            afternoon: buttonStates.afternoon,
+            evening: buttonStates.evening,
+            before: checkBox.before,
+            after: checkBox.after,
+            time: time
+        };
+        const updatedMediData = {
+          ...mediData,
+          detail: infoData
+        };
+        setMediData(updatedMediData);
+        console.log("약 정보: ",updatedMediData); // 업데이트된 mediData 객체를 콘솔에 출력
+        fetchData(updatedMediData); // 저장과 함께 이동
+    };
+
+    const [buttonStates, setButtonStates] = useState({ //아침 점심 저녁
+        morning: false,
+        afternoon: false,
+        evening: false 
+    });
+
+    const toggleButton = (buttonName) => {
+        setButtonStates(prevState => ({
+            ...prevState,
+            [buttonName]: !prevState[buttonName]
+        }));
+    };
+
+    const [checkBox, setCheckBox] = useState({  //식 전후
+        before: false,
+        after: false
+    });
+
+    const toggleCheckBox = (checkBoxName) => { 
+        setCheckBox(prevState => ({
+            ...prevState,
+            [checkBoxName]: !prevState[checkBoxName]
+        }));
+    };
+
+    useEffect(() => {
+      console.log('현재 상태:', checkBox);
+  }, [checkBox]); // buttonStates 상태가 변경될 때마다 실행
+
+    const [time, setTime] = useState(''); //매 time 시간마다
+
+    const handleTimeChange = (event) => {
+        setTime(event.target.value);
+    };
+
 
     return (
         <div className="detailPage">
-
-        <div className="detail_top">
-          <h1>상세 정보<button className="back_button" onClick={()=>goToMypage()}>
-            <img className="backButton_img" src="/backButton.png" alt="back Button"/></button>
-          </h1>
-          <br></br>
-        </div>
-        <div className="detail_list">
-            <div className="detail_text">
-                <h2>먹는 약</h2>
-                <input type="text" name="mediName" value={mediData.mediName} onChange={handleChange} />
+            <div className="detail_top">
+                <h1>상세 정보<button className="back_button" onClick={goToMypage}>
+                    <img className="backButton_img" src="/backButton.png" alt="back Button"/></button>
+                </h1>
+                <br />
             </div>
-                <br></br>
-            <div className="detail_text">
-                <h2>설정한 시간</h2>
-                <input type="text" name="time" value={mediData.time} onChange={handleChange} />
+            <div className="detail_list">
+                <div className="detail_text">
+                    <h2>먹는 약</h2>
+                    <input type="text" name="mediName" value={mediData.mediName} onChange={handleChange} />
+                </div>
+                <br />
+                <div className="detail_text">
+                    <h2>설정한 시간</h2>
+                    <input type="text" name="time" value={mediData.time} onChange={handleChange} />
+                </div>
+                <br />
+                <div className="detail_text_no-border">
+                    <h2>복용법</h2>
+                    <div>
+                        <button onClick={() => toggleButton('morning')} style={{ backgroundColor: buttonStates.morning ? ' rgb(88, 148, 218)' : 'white', color: buttonStates.morning ? 'white' : 'black' }}>
+                            아침
+                        </button>
+                        <button onClick={() => toggleButton('afternoon')} style={{ backgroundColor: buttonStates.afternoon ? ' rgb(88, 148, 218)' : 'white', color: buttonStates.afternoon ? 'white' : 'black' }}>
+                            점심
+                        </button>
+                        <button onClick={() => toggleButton('evening')} style={{ backgroundColor: buttonStates.evening ? ' rgb(88, 148, 218)' : 'white', color: buttonStates.evening ? 'white' : ' black' }}>
+                            저녁
+                        </button>
+                        <br />
+                        <input type="checkbox" checked={checkBox.before} onChange={() => toggleCheckBox('before')} />
+                        <label>식전 30분</label>
+                        <input type="checkbox" checked={checkBox.after} onChange={() => toggleCheckBox('after')} />
+                        <label>식후 30분</label>
+                        <br />
+                        매<input type="text" value={time} onChange={handleTimeChange} placeholder="시간 입력" />
+                        시간마다 의사 지시대로
+                    </div>
+                </div>
             </div>
-                <br></br>
-            <div className="detail_text_no-border">
-                <h2>복용법</h2>
-                <AddInfo/>
-            </div>
-        </div>
-    
-        <button onClick={fetchData} className="save_button" ><h2>저장</h2></button>
-    </div> 
+            <button onClick={handleSave} className="save_button" ><h2>저장</h2></button>
+        </div> 
     );
 }
-
-function AddInfo(){
-
-    const [buttonStates, setButtonStates] = useState({
-      morning: false,
-      afternoon: false,
-      evening: false
-    });
-    
-    const toggleButton = (buttonName) => {
-      setButtonStates(prevState => ({
-        ...prevState,
-        [buttonName]: !prevState[buttonName]
-      }));
-    };
-  
-    const [checkBox, setcheckBox] = useState({
-      before: false,
-      after: false
-    })
-  
-    const toggleCheckBox = (checkBoxName)=>{
-      setcheckBox(prevState=> ({
-        ...prevState,
-        [checkBoxName]: !prevState[checkBoxName]
-      }));
-    };
-  
-      // 시간 입력을 위한 상태와 상태 변경 함수
-      const [time, setTime] = useState('');
-  
-      const handleTimeChange = (event) => {
-        setTime(event.target.value);
-      };
-    
-      return (
-        <div>
-          <button onClick={() => toggleButton('morning')} style={{ backgroundColor: buttonStates.morning ? ' rgb(88, 148, 218)' : 'white', color: buttonStates.morning ? 'white' : 'black' }}>
-            아침
-          </button>
-  
-          <button onClick={()=>toggleButton('afternoon')} style={{ backgroundColor: buttonStates.afternoon ? ' rgb(88, 148, 218)' : 'white', color: buttonStates.afternoon ? 'white' : 'black' }}>
-            점심
-          </button>
-  
-          <button onClick={()=>toggleButton('evening')} style={{ backgroundColor: buttonStates.evening ? ' rgb(88, 148, 218)' : 'white', color: buttonStates.evening ? 'white' : ' black' }}>
-            저녁
-          </button>
-          <br />
-  
-          <input type="checkbox" checked={checkBox.after} onChange={()=>toggleCheckBox('after')}/>
-          <label>식후 30분</label>
-  
-          <input type="checkbox" checked={checkBox.before} onChange={()=>toggleCheckBox('before')}/>
-          <label>식전 30분</label>
-  
-          <br /> 매<input type="text" value={time} onChange={handleTimeChange} placeholder="시간 입력" />
-          시간마다 의사 지시대로 
-  
-        </div>
-      );
-  }
 
 export default AddMedi;
