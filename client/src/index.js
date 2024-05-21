4f9c29490dc0be30a8121379822df93d37082e0d
GAUDUL <limjsu12@naver.com>
2024-05-22 01:30:21 +0900

    [FEAT] Setting environment variable

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './components/index.css';
 //import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter를 import합니다.
 import App from './components/App';
 import reportWebVitals from './components/reportWebVitals';
+require('dotenv').config();
 
 
 
