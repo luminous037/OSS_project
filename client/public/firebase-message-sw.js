f99868f5dfb421a48b096d9a623a98cb6d138afc
GAUDUL <limjsu12@naver.com>
2024-05-29 13:21:34 +0900

    [FEAT] serviceWorker setting for custom

--- client/public/firebase-message-sw.js
+++ client/public/firebase-message-sw.js
@@ -1,32 +1,44 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
+
+const messaging = firebase.messaging();
+
 self.addEventListener("install", function (e) {
-    console.log("fcm sw install..");
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", function (e) {
-    console.log("fcm sw activate..");
-  });
-  
-  self.addEventListener("push", function (e) {
-    console.log("push: ", e.data.json());
-    if (!e.data.json()) return;
-  
-    const resultData = e.data.json().notification;
-    const notificationTitle = resultData.title;
-    const notificationOptions = {
-      body: resultData.body,
-      icon: resultData.image,
-      tag: resultData.tag,
-      ...resultData,
-    };
-    console.log("push: ", { resultData, notificationTitle, notificationOptions });
-  
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
-  
-  self.addEventListener("notificationclick", function (event) {
-    console.log("notification click");
-    const url = "/";
-    event.notification.close();
-    event.waitUntil(clients.openWindow(url));
-  });
+  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
+
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
+  const notificationOptions = {
+    body: resultData.body,
+    icon: resultData.image,
+    tag: resultData.tag,
+    ...resultData,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener("notificationclick", function (event) {
+  const url = "/";
+  event.notification.close();
+  event.waitUntil(clients.openWindow(url));
+});
