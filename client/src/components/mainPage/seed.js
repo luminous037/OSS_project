5f49e3747b16464cf62a0e205be98bc359472280
minseonju <10sc1108@naver.com>
2024-06-03 22:35:56 +0900

    [CHORE]다른 페이지 이동한 후 다시 원래 페이지로 돌아왔을 때도 씨앗 상태 저장되도록 수정

--- client/src/components/mainPage/seed.js
+++ client/src/components/mainPage/seed.js
@@ -4,98 +4,88 @@ import seed from '../image/seed.png';
 import sprout from '../image/sprout.png';
 import flower from '../image/flower5.png';
 import tree from '../image/tree.png';
-import rewardTree from '../image/reward.png'; 
+import rewardTree from '../image/reward.png';
 import InstructionModal from './Guidebook.js';
 import star2 from '../image/star2.png';
 import flowerly from '../image/flower.png';
 
-
-function Seed({ rainCount, setRainCount }) { // rainCount 상태와 함께 setRainCount 함수를 prop으로 받음 prop로 받아야 다른 js 파일에서 만든 함수들을 해당 js(페이지)에서도 사용할 수 있음.
+function Seed({ rainCount, setRainCount }) {
   const [isseedModalOpen, setIsseedModalOpen] = useState(false); //씨앗 모달창 관리
   const [isModalOpen, setIsModalOpen] = useState(false);//설명창 모달
   const [isMoneyModalOpen, setIsMoneyModalOpen] = useState(false); // 보유 금액 모달 상태 추가
-  const [selectedSeed, setSelectedSeed] = useState();  //씨앗 심은 상태 저장
-  const [seedStage, setSeedStage] = useState();  //씨앗의 성장 상태를 저장
+  const [selectedSeed, setSelectedSeed] = useState(null); //씨앗 심은 상태 저장
+  const [seedStage, setSeedStage] = useState(null); //씨앗의 성장 상태를 저장
 
   const [userData, setUserData] = useState({
-      plant: '',
-      rain: 0,
-      point: 0
-  })
+    plant: '',
+    point: 0
+  });
 
-  /*씨앗 종류- 씨앗 모달창에서 씨앗 종류를 나타내기 위해서 구현*/
   const seeds = [
     { id: 1, name: '평범한 씨앗', imageUrl: seed },
     { id: 2, name: '별 씨앗', imageUrl: star2 },
     { id: 3, name: '노란 씨앗', imageUrl: flowerly }
   ];
 
-
-   /*씨앗을 심었는지 여부, 씨앗의 상태(물을 준 횟수에 따른), 보유한 금액을 로컬 저장소에 저장*/
-
-  useEffect(() => { //유저 정보
+  useEffect(() => {
     fetch('/userProfile')
-    .then(response => response.json())
-    .then(data => {
-      console.log('Fetched user data:', data); // 서버에서 받은 데이터 출력
-      const user ={
-       plant: data[0].plant,
-       point: data[0].points !== null ? parseInt(data[0].points, 10) : 0
-      } 
-      setUserData(user);
-    })
-    .catch(error => {
+      .then(response => response.json())
+      .then(data => {
+        console.log('Fetched user data:', data); // 서버에서 받은 데이터 출력
+        const user = {
+          plant: data[0].plant,
+          point: parseInt(data[0].points, 10)
+        };
+        setSelectedSeed(user.plant);
+        setUserData(user);
+        console.log(user);
+      })
+      .catch(error => {
         console.error('유저 정보를 가져오는 중 에러:', error);
-    });
+      });
   }, []);
 
-  const updateUserData = (newData) => {
-    console.log('NewData: ',newData);
-    fetch(`http://localhost:4000/plantUpdate`, {
+  const updateUserData = (seed) => {
+    fetch('http://localhost:4000/plantUpdate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(({ plant: newData.plant ,point: newData.point }))
+      body: JSON.stringify({ plant: seed, point: userData.point })
     })
-    .then(() => {
-    })
-    .catch(err => {
-      console.error('userUpdate중 오류: ', err);
-    });
-  }
+      .then(() => {
+        console.log('User data updated');
+      })
+      .catch(err => {
+        console.error('userUpdate중 오류: ', err);
+      });
+  };
 
-  /*씨앗 모달창 구현*/
+  useEffect(()=>{
+    if(userData.point==0) return;
+    else updateUserData(userData.plant);
+  },[userData.point])
+
   const toggleModal = () => {
     setIsseedModalOpen(!isseedModalOpen);
   };
-  /*주머니(보유 금액) 모달창 구현*/
+
   const toggleMoneyModal = () => {
     setIsMoneyModalOpen(!isMoneyModalOpen);
   };
 
-  /*모달창을 열어 씨앗을 선택했을 때 수행하는 과정들을 나타내는 함수*/
   const selectSeed = (seed) => {
     setSeedStage('seed'); //씨앗의 상태를 저장함
     setSelectedSeed(seed); // 선택된 씨앗을 상태에 저장
-    setUserData((prevState) => {
-      const newData={
+    setUserData(prevState => ({
       ...prevState, // 이전 상태를 복사
-      plant : seed
-    }
-      updateUserData(newData);
-      return newData;
-   });
-    toggleModal(); //모달창
+      plant: seed.id
+    }));
+    updateUserData(seed.id);
+    toggleModal(); // 모달창 닫기
   };
 
-  /*물을 준 횟수에 따른 씨앗의 상태 관리*/
   useEffect(() => {
-    // console.log('rain 상태: ',userData.rain);
-    // console.log('plant 상태: ',userData.plant);
-    // console.log('point 상태: ',userData.point);
-    // console.log('seedState 상태: ',seedStage);
-
     if (selectedSeed && seedStage !== 'rewardTree') {
       if (rainCount >= 4) {
         setSeedStage('rewardTree');
@@ -109,53 +99,41 @@ function Seed({ rainCount, setRainCount }) { // rainCount 상태와 함께 setRa
         setSeedStage('seed');
       }
     }
-  }, [rainCount, selectedSeed, seedStage]);
+  }, [rainCount, seedStage]);
 
-
-  /*물을 4번 준 후 보상을 얻을 수 있는 상태가 되었을 때 수행하는 과정을 나타내는 함수*/
   const handleHarvest = () => {
     setSeedStage('seed');
-    setUserData((prevUserData) => {
-      const newData={
+    setSelectedSeed(null);
+    setRainCount(0); // reset rain count in parent component
+    setUserData(prevUserData => ({
       ...prevUserData, // 이전 상태를 복사
       plant: '',
       point: prevUserData.point + 100 // 이전 돈에 100 추가
-      }
-      setRainCount(0);
-      updateUserData(newData);
-      return newData;
-    });
+    }));
   };
 
-  /*물을 준 횟수에 따른 씨앗의 상태을 이미지로 나타내고 싶어 구현한 함수*/
   const renderSeedStage = () => {
     switch (seedStage) {
-      //물을 1회 준 상태이자 새싹 상태
       case 'sprout':
         return <img src={sprout} alt="새싹 이미지" className="sprout" />;
-        //물을 2회 준 상태이자 꽃 상태
       case 'flower':
         return <img src={flower} alt="꽃 이미지" className="flower" />;
-        //물을 3회 준 상태이자 나무 상태
       case 'tree':
         return <img src={tree} alt="나무 이미지" className="tree" />;
-        //물을 4회 준 상태이자 보상이 열린 상태
       case 'rewardTree':
         return (
           <div>
             <img src={rewardTree} alt="보상 열린 나무 이미지" className="reward-tree" />
-            <button className="accept" onClick={handleHarvest}>보상 얻기</button> 
+            <button className="accept" onClick={handleHarvest}>보상 얻기</button>
           </div>
         );
-        default:
-          return selectedSeed ? <img src={selectedSeed.imageUrl} alt="씨앗 이미지" className="seed-planted" /> : null; // 선택된 씨앗이 존재할 때만 이미지 표시
-      }
+      default:
+        return selectedSeed ? <img src={selectedSeed.imageUrl} alt="씨앗 이미지" className="seed-planted" /> : null; // 선택된 씨앗이 존재할 때만 이미지 표시
+    }
   };
 
   return (
     <div className="Seed">
-     
-
       {isseedModalOpen && (
         <div className="modaldal">
           <p5>•--------------•</p5>
@@ -164,48 +142,37 @@ function Seed({ rainCount, setRainCount }) { // rainCount 상태와 함께 setRa
             {seeds.map((seed) => (
               <li key={seed.id} onClick={() => selectSeed(seed)} className="seed-list-item">
                 <div className="seed-image-container">
-                  <img src={seed.imageUrl} alt={`${seed.name} 이미지`} className="seed-image"/>
+                  <img src={seed.imageUrl} alt={`${seed.name} 이미지`} className="seed-image" />
                 </div>
                 <span>{seed.name}</span>
               </li>
-              
             ))}
-            <button  className="seedcancelbutton" onClick={toggleModal}>[닫기]</button> 
+            <button className="seedcancelbutton" onClick={toggleModal}>[닫기]</button>
           </ul>
-          
         </div>
       )}
 
-
-{isMoneyModalOpen && (
+      {isMoneyModalOpen && (
         <div className="modaldal">
           <p3>•--------------•</p3>
           <h7>💰 보유 금액 💰</h7>
           <p3>보유 금액: {userData.point}</p3>
-          <button className="moneycancelbutton"onClick={toggleMoneyModal}>[닫기]</button> {/* 닫기 버튼 추가 */}
+          <button className="moneycancelbutton" onClick={toggleMoneyModal}>[닫기]</button>
         </div>
       )}
 
-      {userData.plant && (
+      {selectedSeed && (
         <div>
           {renderSeedStage()}
-          {/*<p>선택한 씨앗: {selectedSeed.name}</p>*/}
-          
-          
         </div>
-        
       )}
-      
-      
 
       <div className="sign-container">
-      <button className="plantbutton" onClick={toggleModal}></button>
-      <button className="moneybutton" onClick={toggleMoneyModal}></button> 
-      <InstructionModal isOpen={isModalOpen} close={() => setIsModalOpen(false)} />
-      <button className="button-hover" onClick={() => setIsModalOpen(true)}></button>
+        <button className="plantbutton" onClick={toggleModal}></button>
+        <button className="moneybutton" onClick={toggleMoneyModal}></button>
+        <InstructionModal isOpen={isModalOpen} close={() => setIsModalOpen(false)} />
+        <button className="button-hover" onClick={() => setIsModalOpen(true)}></button>
       </div>
-      
-      
     </div>
   );
 }
