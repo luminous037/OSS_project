07a9609e5c4c119443838d4468c863b67c5f2991
minseonju <10sc1108@naver.com>
2024-06-03 19:36:09 +0900

    [FIX]네비게이션 바가 보이지 않는 오류 수정

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './components/index.css';
 import App from './components/App';
 import reportWebVitals from './components/reportWebVitals';
+import {CookiesProvider} from 'react-cookie'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <App/> 
+        <CookiesProvider>
+                <App/> 
+        </CookiesProvider>
+      
 );
 
 reportWebVitals();
