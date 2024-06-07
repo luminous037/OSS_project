7cafdf6d3a9d2e77a8f7b4814189ba1ba0c6b2dc
Jiji <luminous037@gmail.com>
2024-06-08 01:17:03 +0900

    [feat] 앱 다운로드 안내 모달창을 추가

--- client/src/components/InfoPage/InfoPage_1.js
+++ client/src/components/InfoPage/InfoPage_1.js
@@ -2,17 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './InfoPage_1.css';
 import { initializeFirebase } from './../../PushAlarmSetting';
-import { usePrompt } from '../../PromptContext';  // 경로 수정
+import { usePrompt } from '../../PromptContext';
 
 function InfoPage_1() {
-  const [childName, setChildName] = useState({
-    userName: ''
-  });
+  const [childName, setChildName] = useState({ userName: '' });
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const [warningMessage, setWarningMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
+
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+useEffect(() => {
+  window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    setDeferredPrompt(event);
+  });
+}, []);
 
   const navigate = useNavigate();
-  const { deferredPrompt } = usePrompt();
+
 
   const handleAddToHomeScreen = () => {
     if (deferredPrompt) {
@@ -20,6 +29,7 @@ function InfoPage_1() {
       deferredPrompt.userChoice.then(choiceResult => {
         if (choiceResult.outcome === 'accepted') {
           console.log('User accepted the A2HS prompt');
+          setShowModal(false);
         } else {
           console.log('User dismissed the A2HS prompt');
         }
@@ -72,6 +82,11 @@ function InfoPage_1() {
     }
   }, [childName.userName]);
 
+  // 페이지 로드 시 모달을 표시하기 위한 useEffect
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
+
   const nameSave = () => {
     fetch('http://localhost:4000/saveName', {
       method: 'POST',
@@ -90,6 +105,14 @@ function InfoPage_1() {
       });
   };
 
+  const handleCloseModal = () => {
+    const userChoice = window.confirm('앱을 다운하지 않으면 알람을 사용하지 못할 수 있습니다.');
+    if (userChoice) {
+      setShowModal(false);
+      setShowWarning(false);
+    }
+  };
+
   return (
     <div className="Page1">
       <div className='title_info'>
@@ -106,19 +129,37 @@ function InfoPage_1() {
           maxLength={7} // 최대 입력 길이를 7로 설정
         />
       </div>
-      {warningMessage && <div className="warning">{warningMessage}</div>} {
-      }
+      {warningMessage && <div className="warning">{warningMessage}</div>}
       <div className="text3">
         어린이
       </div>
 
-      {deferredPrompt && (
-        <button className="add-to-home-screen-button" onClick={handleAddToHomeScreen}>Add to Home Screen</button>
-      )}
-
       <div className="navigator">
         <button onClick={nameSave} className="nav-item" disabled={isButtonDisabled}>다음</button>
       </div>
+
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal_info1">
+            <div className="moda_info1_content">
+              <h2>MeddyBaby를 <br />앱으로 사용합니다.</h2>
+              <p>앱을 다운로드하여 알람소리를 허용합니다.</p>
+              
+              {deferredPrompt && (
+              <button  onClick={handleAddToHomeScreen}className='modal-button'>다운로드로 이동</button>
+               )}
+
+              <button onClick={handleCloseModal} className="modal-button">알람소리를 허용하지 않습니다.</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showWarning && (
+        <div className="warning">
+          앱을 다운하지 않으면 알람을 사용하지 못할 수 있습니다.
+        </div>
+      )}
     </div>
   );
 }
